333cec67472daa3336fd2fc02ac269a322242c20
Jhon Barrantes <jbarrantes@board.pe>
2022-06-21 00:25:47 -0500

    Feat: Connect with mongodb

--- src/routers/sessionRouter.js
+++ src/routers/sessionRouter.js
@@ -1,18 +1,50 @@
 import express from 'express'
-import sessions from '../data/sessions.json' assert {type: 'json'};
+import { MongoClient, ObjectId } from 'mongodb'
+import createDebug  from 'debug'
+const debug = createDebug ('app:sessionRouter')
+
+const url = 'mongodb://bqsistemas:barrantes@127.0.0.1:27017'
+const dbName = 'globomantics'
 
 const sessionRouter = express.Router()
 
 sessionRouter.route('/').get((req,res) => {
-    res.render('sessions', {
-        sessions
-    })
+    (async function mongo(){
+        let client;
+        try{
+            client = await MongoClient.connect(url)
+            debug('Connected to the mongo DB')
+
+            const db = client.db(dbName)
+            const sessions = await db.collection('sessions').find().toArray()
+
+            res.render('sessions', {
+                sessions
+            })
+        }catch(error){
+            debug(error.stack)
+        }
+    }())
 })
 sessionRouter.route('/:id').get((req,res) => {
-    const id = req.params.id
-    res.render('session', {
-        session: sessions.find(x => x.id.toString() === id)
-    })
+    (async function mongo(){
+        const id = req.params.id
+        let client;
+        try{
+            client = await MongoClient.connect(url)
+            debug('Connected to the mongo DB')
+
+            const db = client.db(dbName)
+
+            const session = await db.collection('sessions').findOne({ _id: ObjectId(id) })
+            
+            res.render('session', {
+                session
+            })
+        }catch(error){
+            debug(error.stack)
+        }
+    }())
 })
 
 export default sessionRouter
